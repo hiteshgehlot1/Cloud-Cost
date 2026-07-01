889bc88a031d67d0ca5930e6cdfa01a3d71b4e76
Hitesh Gehlot <mrhiteshgehlot@gmail.com>
2026-07-01 15:15:50 +0530

    Change API base URL to production environment

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://cloud-spend-backend.onrender.com'
 });
 
 export const calculateCost = async (data) => {
